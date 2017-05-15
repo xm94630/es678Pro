2020f0f906590bb330778651b56957767cf0d125
xm94630 <xm94630@126.com>
2017-05-15 16:07:43 +0800

    css

--- app/index.js
+++ app/index.js
@@ -1,2 +1,26 @@
-const fn = () => console.log('啦啦啦，啦啦啦~'); 
-fn();
+var l = function(){
+    return console.log.apply(console,arguments);
+}
+
+var bee = ((bee)=>{
+    
+    /*
+     * 案例1
+     */
+    bee.caseA1 = (()=>{
+        l(123);
+    })
+
+
+    return bee;
+})(bee||{});
+
+
+
+
+
+
+
+
+
+
