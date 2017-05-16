0064165308e895c6c27e216fa48aeed95d6cdc93
xm94630 <xm94630@126.com>
2017-05-16 17:24:00 +0800

    添加案例 await

--- app/index.js
+++ app/index.js
@@ -13,9 +13,9 @@ var bee = ((bee)=>{
         l('我是箭头函数');
     })
 
+
     /*
-     * 案例2 promise
-     * 这个是ES2017
+     * 案例2 ES6 promise
      */
     bee.caseA2 = (()=>{
 
@@ -33,9 +33,43 @@ var bee = ((bee)=>{
         })
 
         //通过这个同步的语句，我们可以发现，promise中的内容都是异步的
-        l('我是同步代码3333')
+        l('我是同步代码')
+    })
 
-    })()
+
+    /*
+     * 案例3 ES8 promise + async + await
+     */
+    bee.caseA3 = (()=>{
+
+        function resolveAfterInterval(x) {
+            l('==>函数调用')
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    resolve(x * x);
+                },1000);
+            });
+        }
+
+        //注意当函数内部有await出现的时候，在外层函数之前要标记 async
+        async function add() {
+            //注意，这里还是同步的
+            //包括第一次执行 resolveAfterInterval 函数的时候，也是同步的呢
+            l('add函数：这里其实还是同步的！')
+
+            //如何理解 await 呢，我可以认为这个 await 是在等待 promise的发生！而且具有阻塞的作用
+            var a = await resolveAfterInterval(1); 
+            //只有上面这个值返回了，才会继续
+            var b = await resolveAfterInterval(2);
+            return a+b;
+        }
+        add().then((v)=>console.log('异步地得到最后的结果：'+v))
+
+        l('我是同步代码')
+    })
+
+
+
 
 
     return bee;
